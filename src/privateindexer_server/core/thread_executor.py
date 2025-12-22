5c544e4686bbdbdb15f6418c4d2209681b0d632a
David Hume <xenophilicy@gmail.com>
2025-12-21 22:53:07 -0600

    feat(core): implement hash executor process pool for database check task

--- src/privateindexer_server/core/thread_executor.py
+++ src/privateindexer_server/core/thread_executor.py
@@ -1,5 +1,17 @@
 from concurrent.futures.process import ProcessPoolExecutor
 
 from privateindexer_server.core.config import MAX_THREADS
+from privateindexer_server.core.logger import log
 
-EXECUTOR = ProcessPoolExecutor(max_workers=MAX_THREADS)
+_hash_executor: ProcessPoolExecutor | None = None
+
+
+def get_hash_executor() -> ProcessPoolExecutor:
+    """
+    Create or return an existing process pool executor for hashing files
+    """
+    global _hash_executor
+    if _hash_executor is None or _hash_executor._shutdown_thread:
+        _hash_executor = ProcessPoolExecutor(max_workers=MAX_THREADS)
+        log.debug("[EXECUTOR] Spawned new hash executor")
+    return _hash_executor
