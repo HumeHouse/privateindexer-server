5ae2d5f4e680f8ebdc744f5542b7c895381903ff
David Hume <xenophilicy@gmail.com>
2026-01-17 10:34:29 -0600

    feat(core): improve stale torrent check logic and purge torrent file during deletion

--- src/privateindexer_server/core/stale_check.py
+++ src/privateindexer_server/core/stale_check.py
@@ -1,7 +1,8 @@
 import asyncio
 import datetime
+import os
 
-from privateindexer_server.core import mysql
+from privateindexer_server.core import mysql, utils
 from privateindexer_server.core.config import STALE_CHECK_INTERVAL, STALE_THRESHOLD
 from privateindexer_server.core.logger import log
 
@@ -16,8 +17,22 @@ async def periodic_stale_check_task():
             log.info("[STALE-CHECK] Running stale torrents check")
             before = datetime.datetime.now()
 
-            # purge torrents which have not been seen in at least STALE_THRESHOLD number of seconds
-            removed_torrents = await mysql.execute("DELETE FROM torrents WHERE last_seen < NOW() - INTERVAL %s SECOND", (STALE_THRESHOLD,), include_row_count=True)
+            # fetch torrents which have not been seen in at least STALE_THRESHOLD number of seconds
+            stale_torrents = await mysql.fetch_all("SELECT id, hash_v2 FROM torrents WHERE last_seen < NOW() - INTERVAL %s SECOND", (STALE_THRESHOLD,))
+
+            removed_torrents = 0
+
+            # loop through each stale torrent to remove the file if it exists
+            for stale_torrent in stale_torrents:
+                torrent_file = utils.get_torrent_file(stale_torrent["hash_v2"])
+
+                # remove the file
+                if os.path.exists(torrent_file):
+                    os.unlink(torrent_file)
+
+                # remove from database
+                await mysql.execute("DELETE FROM torrents WHERE id = %s", (stale_torrent["id"],))
+                removed_torrents += 1
 
             delta = datetime.datetime.now() - before
             log.info(f"[STALE-CHECK] Stale torrents check complete ({delta}): purged {removed_torrents} torrents")
